2670bcdd28b5d1f3e141fc7138b74634aa60d17f
beatorizu <betrizaf@gmail.com>
2020-05-18 13:10:48 -0300

    test: Add testid

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Contacts extends React.Component {
 	render() {
 		return (
-      <div className="container">
+      <div className="container" data-testid="contacts">
         <section className="contacts">
           <article className="contact">
             <span className="contact__avatar" />
