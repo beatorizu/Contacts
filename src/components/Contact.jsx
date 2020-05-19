9d1e66b83b25dc662717af2ad01f881738444455
beatorizu <betrizaf@gmail.com>
2020-05-19 16:42:21 -0300

    fix: Format when admission date is not empty

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 class Contact extends React.Component {
   render() {
     const { avatar, name, phone, country, admissionDate, company, department } = this.props.data;
+    const date = !admissionDate ? "" : new Intl.DateTimeFormat('pt-BR').format(new Date(admissionDate))
 
     return (
       <article className="contact" data-testid="contact">
@@ -10,7 +11,7 @@ class Contact extends React.Component {
         <span className="contact__data">{name}</span>
         <span className="contact__data">{phone}</span>
         <span className="contact__data">{country}</span>
-        <span className="contact__data">{new Intl.DateTimeFormat('pt-BR').format(new Date(admissionDate))}</span>
+        <span className="contact__data">{date}</span>
         <span className="contact__data">{company}</span>
         <span className="contact__data">{department}</span>
       </article>
