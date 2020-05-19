591750807d3df47d1f44de9b139badfd917d4272
beatorizu <betrizaf@gmail.com>
2020-05-19 11:44:00 -0300

    refactor: Create contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Contact extends React.Component {
   render() {
-    return (<div />);
+    const { avatar, name, phone, country, admissionDate, company, department } = this.props;
+
+    return (
+      <article className="contact" data-testid="contact">
+        <span className="contact__avatar"><img src={avatar} alt="" /></span>
+        <span className="contact__data">{name}</span>
+        <span className="contact__data">{phone}</span>
+        <span className="contact__data">{country}</span>
+        <span className="contact__data">{admissionDate}</span>
+        <span className="contact__data">{company}</span>
+        <span className="contact__data">{department}</span>
+      </article>
+    );
   }
 }
 
