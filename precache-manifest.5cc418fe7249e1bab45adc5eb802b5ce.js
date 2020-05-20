042d92892e645d02c962c021ea4def1c0337c06a
beatorizu <betrizaf@gmail.com>
2020-05-20 17:10:39 -0300

    Updates

---
+++ precache-manifest.5cc418fe7249e1bab45adc5eb802b5ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32bc75136e7b57bf4365f863b1881115",
+    "url": "/Contacts/index.html"
+  },
+  {
+    "revision": "624f7340c3a264c65efb",
+    "url": "/Contacts/static/css/main.b716568e.chunk.css"
+  },
+  {
+    "revision": "ba50e33cbdb28455d147",
+    "url": "/Contacts/static/js/2.50e2e3c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Contacts/static/js/2.50e2e3c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624f7340c3a264c65efb",
+    "url": "/Contacts/static/js/main.09ba630a.chunk.js"
+  },
+  {
+    "revision": "e5b4252fe22d8343e796",
+    "url": "/Contacts/static/js/runtime-main.65ef5acc.js"
+  },
+  {
+    "revision": "82a7c06238e831cd8f70bf1182156c2e",
+    "url": "/Contacts/static/media/logo.82a7c062.svg"
+  }
+]);
