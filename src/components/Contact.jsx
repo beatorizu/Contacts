4a8eeeb9362325cd6e67dce5f9c29996d0e11ca5
beatorizu <betrizaf@gmail.com>
2020-05-20 14:04:44 -0300

    test: Add testid

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,11 +8,11 @@ class Contact extends React.Component {
     return (
       <article className="contact" data-testid="contact">
         <span className="contact__avatar"><img src={avatar} alt="" /></span>
-        <span className="contact__data">{name}</span>
-        <span className="contact__data">{phone}</span>
-        <span className="contact__data">{country}</span>
-        <span className="contact__data">{date}</span>
-        <span className="contact__data">{company}</span>
+        <span className="contact__data" data-testid="contact-name">{name}</span>
+        <span className="contact__data" data-testid="contact-phone">{phone}</span>
+        <span className="contact__data" data-testid="contact-country">{country}</span>
+        <span className="contact__data" data-testid="contact-date">{date}</span>
+        <span className="contact__data" data-testid="contact-company">{company}</span>
         <span className="contact__data">{department}</span>
       </article>
     );
