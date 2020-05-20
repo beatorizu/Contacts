d453f0ec8945c9db14749f7ba99beb079090f59a
beatorizu <betrizaf@gmail.com>
2020-05-20 11:46:33 -0300

    test: Check the contact content

--- src/__tests__/submit.test.js
+++ src/__tests__/submit.test.js
@@ -1,28 +1,52 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from 'react'
+import { render, waitForElement } from '@testing-library/react'
 
+import App from '../App';
 import Contact from '../components/Contact';
 import Contacts from '../components/Contacts';
 import Filters from '../components/Filters';
 import Topbar from '../components/Topbar';
 
-const mockUserData = {
-	avatar: '',
-	name: '',
-	phone: '',
-	country: '',
-	admissionDate: '',
-	company: '',
-	department: '',
+import mockContactsList from './mock/contacts';
+
+const mockUserContact = { ...mockContactsList[0] };
+
+const state = {
+	contacts: [...mockContactsList],
+	hasSearchFilter: false,
+	searchFilter: [],
+	sortBy: '',
+	sortedContacts: [],
 };
 
 describe('Contacts APP', () => {
+	it('Should render APP', async () => {
+		const renderWithState = ({ state }) => render(<App />);
+
+		const { getByTestId } = renderWithState({ state });
+		const appContainer = getByTestId('app');
+
+		expect(appContainer).toHaveClass('app');
+		expect(appContainer.children.length).toBe(3);
+	});
+
 	it('Should render Contact component', () => {
-		const { getByTestId } = render(<Contact data={mockUserData}/>);
+		const { getByTestId } = render(<Contact data={mockUserContact} />);
 		const container = getByTestId('contact');
+		const name = getByTestId('contact-name');
+		const phone = getByTestId('contact-phone');
+		const country = getByTestId('contact-country');
+		const date = getByTestId('contact-date');
+		const company = getByTestId('contact-company');
 
 		expect(container).toHaveClass('contact');
 		expect(container.children.length).toBe(7);
+
+		expect(name).toHaveTextContent(mockUserContact.name);
+		expect(phone).toHaveTextContent(mockUserContact.phone);
+		expect(country).toHaveTextContent(mockUserContact.country);
+		expect(date).toHaveTextContent('08/04/2019');
+		expect(company).toHaveTextContent(mockUserContact.company);
 	});
 
 	it('Should render Contacts component', () => {
@@ -49,4 +73,3 @@ describe('Contacts APP', () => {
 		expect(container.children.length).toBe(1);
 	});
 });
-
